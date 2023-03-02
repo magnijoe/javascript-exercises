e3eba3f0ca2b447992d140b99ae7a454717ce35a
Magnus Guldager <m@magnusg.dk>
2023-03-02 19:46:43 +0100

    exercises made except getTheOldest

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(numOne, numTwo) {
+    return numOne + numTwo; 
 };
 
-const subtract = function() {
-	
+const subtract = function(num, newNum) {
+    return num - newNum;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+
+return  array.reduce((a, b) => {
+    return a + b;
+  }, 0);
+
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let output = 1;
 
+  for (let i = 0; i < array.length; i++) {
+    output *= array[i];
+  }
+
+  return output
 };
 
-const power = function() {
-	
+const power = function(numOne, numTwo) {
+    return Math.pow(numOne, numTwo);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+
+  let result = num;
+
+  if (num === 0 || num === 1){
+    return 1;
+  }
+
+  while (num > 1) {
+    num--;
+    result = result * num;
+  };
+
+  return result
+
+
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+    var string = string.replace(/[\p{P}\p{S}]/gu, "")
+    
+    var string = string.replace(/ /g, "");
+
+    var string = string.toLowerCase();
+
+    var splitString = string.split("");
+
+    var reverseArray = splitString.reverse();
+
+    var joinArray = reverseArray.join("");
+
+    if (joinArray === string){
+        return true;
+    } else {
+        console.log(joinArray)
+        return false;
+    }
 
 };
 
